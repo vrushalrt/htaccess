b80937c1e47731b7abd28449bfa12c743951680d
vrushalrt <vrushalrt@gmail.com>
2015-12-11 00:43:14 +0530

    initial commit

---
+++ htaccess/test1.php
@@ -0,0 +1,4 @@
+<?php
+echo "Its Test1";
+
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h3>Hey this output based on .htaccess</h3>";
+echo "<p>Generaly this output shown by <b>index.php</b> which located in same directory of .htaccess were exist</p>";
+?>
